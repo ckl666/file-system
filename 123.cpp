7512e810c0498fa5506a3c1cef1a62f22da249b3
ckl <1589983632@qq.com>
2019-08-16 01:28:54 -0700

    add sum

--- 123.cpp
+++ 123.cpp
@@ -2,6 +2,18 @@
 // Author: xiaxiaosheng
 // Created Time: Fri 16 Aug 2019 01:24:07 AM PDT
 
+int max(int a ,int b)
+{
+    return a>b;
+}
+
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+
 int main()
 {
     return 0;
